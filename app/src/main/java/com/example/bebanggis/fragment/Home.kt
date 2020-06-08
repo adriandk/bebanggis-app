9cada57b5e51a24a36440d5a2f1b30d18fabda92
Adrian Daniel Kristanto <adriandaniel1803@gmail.com>
2020-06-08 23:39:51 +0700

    Add Music

--- app/src/main/java/com/example/bebanggis/fragment/Home.kt
+++ app/src/main/java/com/example/bebanggis/fragment/Home.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.bebanggis.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class Home : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
@@ -19,6 +22,10 @@ class Home : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        button_about.setOnClickListener {
+            Navigation.createNavigateOnClickListener(R.id.action_home2_to_about)
+        }
     }
 
 }
